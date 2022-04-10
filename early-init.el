4cc875405fc6177cce188246e1f7b3d22d43a336
Brian O'Reilly <fade@deepsky.com>
2022-04-10 15:44:47 -0400

    emacs was locking up
    I _think_ this was caused by the fact that I had set the heap to a
    very large number, and when emacs finally got around to collecting
    garbage, it took a very long time. up to twenty seconds. Anyhow, leave
    the gc-cons-threshold at default for now.

--- early-init.el
+++ early-init.el
@@ -2,7 +2,7 @@
 ;; Copyright (c) 2018-2021 Brian O'Reilly <fade@deepsky.com>
 
 ;; we have a lot of RAM. Let's use it.
-(setq gc-cons-threshold 400000000)
+;; (setq gc-cons-threshold 400000000)
 
 ;; moving over to straight, because we're tracking emacs early release test tags.
 (setq package-enable-at-startup nil)
